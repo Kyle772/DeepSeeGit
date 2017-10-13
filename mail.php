77503702dafcb23b9b6614e0ff4392e6410e50d1
William Saxton <saxton@gmail.com>
2017-10-13 05:46:24 -0400

    removed top signup background; some mail changes (still broken)

--- mail.php
+++ mail.php
@@ -5,10 +5,17 @@ require 'vendor/autoload.php';
 // If you are not using Composer (recommended)
 // require("path/to/sendgrid-php/sendgrid-php.php");
 
-$from = new SendGrid\Email(null, "test@example.com");
-$subject = "Hello World from the SendGrid PHP Library!";
+if(array_key_exists('message', $_POST)) {
+    $subject = "Message from DeepSee landing page";
+    $content = new SendGrid\Content("text/plain", $_POST["message"]);
+} else {
+    $subject = "Newsletter subscription request from DeepSee landing page";
+    $content = new SendGrid\Content("text/plain", 'Subscribe');
+}
+
+$from = new SendGrid\Email(null, $_POST["email"]);
 $to = new SendGrid\Email(null, "saxton@gmail.com");
-$content = new SendGrid\Content("text/plain", "Hello, Email!");
+
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = getenv('SENDGRID_API_KEY');
