6009866415a253799ec9db47af036b3e6b9d8e93
William Saxton <saxton@gmail.com>
2017-11-07 11:07:02 -0500

    added confirmation pop-up for form submissions

--- mail.php
+++ mail.php
@@ -1,16 +1,16 @@
 <?php
-// If you are using Composer
+
 require 'vendor/autoload.php';
 
-// If you are not using Composer (recommended)
-// require("path/to/sendgrid-php/sendgrid-php.php");
-
 // Send back to home page if not filled out correctly
-if(!isset($_POST['email'])) {
-    header('Location: index.html');
+if(!isset($_POST['email']) || $_POST['email'] == null) {
+    header('Location: /');
+    die();
 }
 
+// set the subject and content fields
 if(isset($_POST['message'])) {
+    $message_type = "contact";
     $subject = "Message from HOME page";
 
     $content_text =<<<EOD
@@ -24,22 +24,40 @@ EOD;
     $content = new SendGrid\Content("text/plain", $content_text);
 
 } else {
+    $message_type = "subscription";
     $subject = "Subscription request from HOME page";
     $content = new SendGrid\Content("text/plain", $_POST['email']);
 }
 
+// set the rest of the email fields
 $contact_email = getenv('CONTACT_EMAIL');
-
 $from = new SendGrid\Email(null, $_POST['email']);
 $to = new SendGrid\Email(null, $contact_email);
-
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
 
 $response = $sg->client->mail()->send()->post($mail);
-// echo $response->statusCode();
+
+// set a cookie with a message that will be displayed
+// in a pop-up after the form is submitted based upon
+// the type and result status
+
 // echo $response->headers();
 // echo $response->body();
-header('Location: index.html');
+if($response->statusCode() == '202') {
+    if($message_type == 'contact') {
+        $message = 'Thank you for your message.  We will respond to you shortly.';
+    } else {
+        $message = 'Thank you for signing up.  We will keep you posted.';
+    }    
+} else {
+    if($message_type == 'contact') {
+        $message = 'Message failed: please email us directly at ' . getenv('CONTACT_EMAIL') . ' for assistance';
+    } else {
+        $message = 'Sign-up failed: please email us directly at ' . getenv('CONTACT_EMAIL') . ' for assistance';
+    }     
+}
+setcookie("message", $message);
+header('Location: /');
